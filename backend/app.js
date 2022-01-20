d802f7e787f1b8bc4f38aec64ca3227a82d51aea
Yakov Shitrit <yakovd33@gmail.com>
2022-01-20 11:44:22 +0200

    .

--- backend/app.js
+++ backend/app.js
@@ -10,9 +10,6 @@ const http = require("http");
 const socketio = require("socket.io");
 
 const app = express();
-// app.listen(app.get('port'), function() {
-// 	console.log('Node app is running on port', app.get('port'));
-// });
 
 // Socket.io
 var chatUsers = {};
